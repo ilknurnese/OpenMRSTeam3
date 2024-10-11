d56e9119df6b3a3561286bce30e01f2504e2a618
betulgozlukoglu <gozlukoglubetul@gmail.com>
2024-10-11 20:39:22 +0300

    user strory 2,5,9 eklendi

---
+++ src/main/us_5.java
@@ -0,0 +1,33 @@
+package main;
+
+import Utility.BaseDriver;
+import Utility.Tools;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class us_5 extends BaseDriver {
+
+        @Test()
+        public void myaccount() {
+            Elements el=new Elements();
+            el.dil.click();
+
+            Tools.Bekle(2);
+            el.en.click();
+
+            Tools.Bekle(2);
+
+            el.demobutton.click();
+            el.enter.click();
+            el.username.sendKeys("admin");
+            el.password.sendKeys("Admin123");
+            el.location.click();
+            el.loginbutton.click();
+            Assert.assertTrue(el.adminassertion.getText().contains("admin"), "login olunmamıştır");
+            el.myaccountadmin.click();
+            el.myaccount.click();
+
+        }
+    }
+
+
